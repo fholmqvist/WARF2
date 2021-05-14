b4e0d4b8c12cceaff3626ffc5e7dfec7da55bdf2
hol@svep.se <hol@svep.se>
2021-05-14 17:48:42 +0200

    Added crumbled wall graphic. Changed names to numeric.

--- worldmap/constants.go
+++ worldmap/constants.go
@@ -13,22 +13,24 @@ const (
 	WallExposed
 	WallSelectedSolid
 	WallSelectedExposed
+	WallCrumbled1
+	WallCrumbled2
 
-	FloorBricksOne = iota + 8
-	FloorBricksTwo
-	FloorBricksThree
-	FloorBricksFour
-	FloorBricksFive
-	FloorBricksSix
-	FloorBricksSeven
-	FloorBricksEight
-	FloorBricksNine
-	FloorBricksTen
+	FloorBricks1 = iota + 8
+	FloorBricks2
+	FloorBricks3
+	FloorBricks4
+	FloorBricks5
+	FloorBricks6
+	FloorBricks7
+	FloorBricks8
+	FloorBricks9
+	FloorBricks10
 
-	WoodFloorOne = iota + 14
-	WoodFloorTwo
-	WoodFloorThree
-	WoodFloorFour
+	WoodFloor1 = iota + 14
+	WoodFloor2
+	WoodFloor3
+	WoodFloor4
 )
 
 const (
